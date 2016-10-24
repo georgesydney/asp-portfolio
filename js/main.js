59cd2afc092adf6ab7daa30b87bc5c6b568441fd
georgesydney <hello@georgesydney.com>
2016-10-24 12:51:37 +0100

    Styled h1

--- js/main.js
+++ js/main.js
@@ -1,6 +1,11 @@
 $(document).ready(function(){
 
-	console.log('it works');
+	// console.log('it works');
+
+	$( window ).resize(function() {
+		
+  		location.reload();
+	});
 
 	$('.slideshow').cycle({
 
