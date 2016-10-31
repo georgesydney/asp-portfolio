02b6a3c1eb911f0191d0aacb8774c0a235480e69
georgesydney <hello@georgesydney.com>
2016-10-31 11:52:30 +0000

    tweaked style of header & added jQuery to hamburger nav

--- js/main.js
+++ js/main.js
@@ -4,4 +4,30 @@ $(document).ready(function(){
 		});
 	});
 
+	$('#content').click(function(){
+
+		if ($('#hamburger-navigation:visible')) {
+
+			$('#hamburger-navigation').hide();
+
+		} else {}
+
+	});
+
+	$(function() {
+		$('a[href*="#"]:not([href="#"])').click(function() {
+	    	if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+	      		var target = $(this.hash);
+	      		target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+	      		if (target.length) {
+	        		$('html, body').animate({
+	          			scrollTop: target.offset().top
+	        			}, 1000);
+	        			return false;
+	      		}
+	    	}
+	  	});
+	});
+
+
 });
